81b70e6e4b8aef8a917cb79e0f38c08ec097332b
Aryaman Dubey <89002729+AryamanDubey28@users.noreply.github.com>
2022-09-28 08:29:38 +0100

    answer

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"Parameter should be a number greater than 0, parameter recieved is {number_of_primes}")
     list = []
+    counter = 2
+    while len(list) != number_of_primes:
+        if isPrimeChecker(counter):
+            list.append(counter)
+        counter+=1
     return list
+
+
+def isPrimeChecker(number):
+    for i in range(1,number):
+        if number % i == 0 and i != 1:
+            return False   
+    return True
+
+print(primes(-5))
